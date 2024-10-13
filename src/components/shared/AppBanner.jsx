f34575ea243098f0fe12bb48a1e800e2b8721b70
hubol01 <kuloleg52@gmail.com>
2024-10-14 00:38:45 +0300

    fix code

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -1,5 +1,5 @@
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
-import { FiArrowDownCircle } from 'react-icons/fi';
+// import { FiArrowDownCircle } from 'react-icons/fi';
 import developerLight from '../../images/developer.svg';
 import developerDark from '../../images/developer-dark.svg';
 import { motion } from 'framer-motion';

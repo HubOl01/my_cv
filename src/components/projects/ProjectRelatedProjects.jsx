fc463e6d19a291aaf50ea5e603a803c25248eff4
hubol01 <kuloleg52@gmail.com>
2024-10-15 15:26:47 +0300

    fix related projects

--- src/components/projects/ProjectRelatedProjects.jsx
+++ src/components/projects/ProjectRelatedProjects.jsx
@@ -5,7 +5,10 @@ import { projectsData } from "../../data/projects";
 import { Link } from "react-router-dom";
 
 const ProjectRelatedProjects = ({ project }) => {
-  // const { singleProjectData } = useContext(SingleProjectContext);
+  // Фильтруем проекты, исключая текущий проект
+  const relatedProjects = projectsData.filter(
+    (otherProject) => otherProject.id !== project.id
+  );
 
   return (
     <div className="mt-10 pt-10 sm:pt-14 sm:mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
@@ -13,30 +16,27 @@ const ProjectRelatedProjects = ({ project }) => {
         Другие проекты
       </p>
 
-      <div className="grid sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4	xl:grid-cols-4 2xl:grid-cols-4 gap-10">
-        {projectsData.map((otherProject) => {
-          return project.id === otherProject.id ? (
-            <></>
-          ) : (
-            <Link to={`/projects/${otherProject.id}`}>
-              <img
-                src={
-                  otherProject.url_background != null
-                    ? otherProject.url_background
-                    : bg_github
-                }
-                className="rounded-xl cursor-pointer border-none w-full"
-                style={{
-                  height: "160px",
-                  objectFit: "cover",
-                  minWidth: "170px",
-                }}
-                alt={otherProject.title}
-                key={otherProject.id}
-              />
-            </Link>
-          );
-        })}
+      <div className="grid grid-cols-1 sm:grid-cols-4 gap-10">
+        {relatedProjects.map((otherProject) => (
+          <Link to={`/projects/${otherProject.id}`} key={otherProject.id}>
+            <img
+              src={
+                otherProject.url_background != null
+                  ? otherProject.url_background
+                  : bg_github
+              }
+              className="rounded-xl cursor-pointer border-none w-full h-full"
+              style={{
+                height: "160px",
+                // minHeight: "160px",
+                maxHeight: "200px",
+                objectFit: "cover",
+                minWidth: "170px",
+              }}
+              alt={otherProject.title}
+            />
+          </Link>
+        ))}
       </div>
     </div>
   );

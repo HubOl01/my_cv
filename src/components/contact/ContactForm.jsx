57b21a5b0f09d0f71b0a94b623f15ae2a92ca6f8
hubol01 <kuloleg52@gmail.com>
2024-10-16 23:47:55 +0300

    fix v2.0

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -103,7 +103,7 @@ const ContactForm = () => {
             ></textarea>
           </div>
 
-          <div className="font-general-medium w-100 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
+          <div className="font-general-medium w-100  text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
             <Button
               title="Отправить сообщение"
               type="submit"
--- src/components/reusable/Button.jsx
+++ src/components/reusable/Button.jsx
@@ -1,5 +1,5 @@
 function Button({ title }) {
-	return <button>{title}</button>;
+  return <button className="w-full h-full px-4 py-2.5">{title}</button>;
 }
 
 export default Button;
